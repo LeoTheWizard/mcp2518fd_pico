3e508c8d83829dddb7998075404afd0eb12af25e
LeoTheWizard <leowalk183@gmail.com>
2025-10-27 19:49:10 +0000

    spi abstraction for raspberry pico

--- src/mcp2518fd.h
+++ src/mcp2518fd.h
@@ -2,3 +2,12 @@
 /// @author Leo Walker
 
 #pragma once
+
+#include <stdint.h>
+
+#include "spi.h"
+
+typedef struct mcp2518fd_device mcp2518fd_device_t;
+
+mcp2518fd_device_t* mcp2518fd_device_init(spi_instance_t* spi_device);
+void mcp2518fd_device_destroy(mcp2518fd_device_t* device);
