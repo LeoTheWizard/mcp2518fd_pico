957ce23eeb7afa087da68fb3e2fb9e4e906c0a8f
LeoTheWizard <leowalk183@gmail.com>
2025-10-27 23:08:36 +0000

    registers from datasheet

--- src/mcp2518fd.h
+++ src/mcp2518fd.h
@@ -4,10 +4,11 @@
 #pragma once
 
 #include <stdint.h>
-
 #include "spi.h"
 
 typedef struct mcp2518fd_device mcp2518fd_device_t;
 
 mcp2518fd_device_t* mcp2518fd_device_init(spi_instance_t* spi_device);
 void mcp2518fd_device_destroy(mcp2518fd_device_t* device);
+
+void mcp2518fd_reset(mcp2518fd_device_t* device);
